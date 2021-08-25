64d131434930f14d5bcdb5359d1603720b85d82f
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-08-26 00:07:29 +0300

    Task is done without optional condition

--- Authorization/MainViewController.swift
+++ Authorization/MainViewController.swift
@@ -12,18 +12,26 @@ class ViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    let trueUserName = "User"
-    let truePassword = "1234"
+    private let trueUserName = "User"
+    private let truePassword = "1234"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.welcomeMessage = "Welcome, \(userNameTF.text ?? "someone")!"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        userNameTF.resignFirstResponder()
+        passwordTF.resignFirstResponder()
+    }
     
     @IBAction func logInButton() {
-//        if userNameTF.text == trueUserName && passwordTF.text == truePassword {
-////            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-////            guard let newVC = storyBoard.instantiateViewController(withIdentifier: "vc") as? WelcomeViewController else { return }
-////            newVC.welcomeMessage = "Welcome, \(userNameTF.text ?? "someone")"
-////            present(newVC, animated: true)
-//        } else {
-//            showAlert(with: "Wrong User Name or Password", and: "Please, enter correct User Name and Password")
-//        }
+        if userNameTF.text == trueUserName && passwordTF.text == truePassword {
+            // Понимаю, что здесь код неверный, но не придумал лучшего варианта
+        } else {
+            showAlert(with: "Wrong User Name or Password", and: "Please, enter correct User Name and Password")
+        }
     }
     
     @IBAction func userNamePromptButton() {
@@ -34,15 +42,18 @@ class ViewController: UIViewController {
         showAlert(with: "Hey there :)", and: "Your password is \(truePassword)")
     }
     
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
     private func showAlert(with title: String, and message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            self.userNameTF.text = ""
             self.passwordTF.text = ""
         }
         alert.addAction(okAction)
         present(alert, animated: true)
     }
-    
 }
 
