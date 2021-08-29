bfc1eeae5e2afaac083be6445cff808755c27d1a
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-08-29 16:36:50 +0300

    All scenes are done

---
+++ Authorization/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutMeViewController.swift
+//  Authorization
+//
+//  Created by Ярослав Бойко on 29.08.2021.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    @IBOutlet var navigationBar: UINavigationItem!
+    @IBOutlet var userInformation: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ Authorization/ViewControllers/HobbiesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HobbiesViewController.swift
+//  Authorization
+//
+//  Created by Ярослав Бойко on 29.08.2021.
+//
+
+import UIKit
+
+class HobbiesViewController: UIViewController {
+
+    @IBOutlet var hobbiesInfo: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
